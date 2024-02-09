0ad3154e3d09e3555685494c81db01a55374baec
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-02-09 16:05:00 +0100

    feat: create lecturer dto

---
+++ src/lecturers/dto/create-lecturer.dto.ts
@@ -0,0 +1,29 @@
+import { Transform } from 'class-transformer';
+import {
+  IsString,
+  IsDate,
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class CreateLecturerDto {
+  @IsString()
+  firstname: string;
+
+  @IsString()
+  lastname: string;
+
+  @Transform(({ value }) => new Date(value))
+  @IsDate()
+  dateOfBirth: Date;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Transform(({ value }) => Number.parseInt(value))
+  @Transform(({ value }) => Boolean(value))
+  @IsBoolean()
+  available: boolean;
+}
