9ce798a31da5fb042eaf7e07ec3b1d2a0b05c73f
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-02-15 11:13:01 +0100

    fix: update services functions return type

--- src/departments/departments.service.ts
+++ src/departments/departments.service.ts
@@ -3,26 +3,26 @@ import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Department } from './schemas/department.schema';
+import { Department, DepartmentDocument } from './schemas/department.schema';
 
 @Injectable()
 export class DepartmentsService {
   constructor(
-    @InjectModel(Department.name) private departmentModel: Model<Department>,
+    @InjectModel(Department.name) private departmentModel: Model<DepartmentDocument>,
   ) {}
 
-  async create(createDepartmentDto: CreateDepartmentDto): Promise<Department> {
+  async create(createDepartmentDto: CreateDepartmentDto): Promise<DepartmentDocument> {
     const newDepartment =
       await this.departmentModel.create(createDepartmentDto);
     return newDepartment;
   }
 
-  async findAll(): Promise<Department[]> {
+  async findAll(): Promise<DepartmentDocument[]> {
     const allDepartments = await this.departmentModel.find({});
     return allDepartments;
   }
 
-  async findOne(id: number): Promise<Department> {
+  async findOne(id: number): Promise<DepartmentDocument> {
     const department = await this.departmentModel.findById(id);
     return department;
   }
