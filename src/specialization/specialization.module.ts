027be379edb65ecf12693ba4ce709d1b63aefc72
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-02-21 05:17:09 +0100

    refactor: remove controller file imports

--- src/specialization/specialization.module.ts
+++ src/specialization/specialization.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SpecializationService } from './specialization.service';
-import { SpecializationController } from './specialization.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import {
   Specialization,
@@ -16,7 +15,6 @@ import {
       },
     ]),
   ],
-  controllers: [SpecializationController],
   providers: [SpecializationService],
   exports: [SpecializationService],
 })
