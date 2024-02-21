29ed29ff275a5b6f55486a6f23b44f4dd414ae7e
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-02-21 04:23:27 +0100

    feat: add courses module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LecturersModule } from './lecturers/lecturers.module';
 import { SpecializationModule } from './specialization/specialization.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/acas'),
     LecturersModule,
     SpecializationModule,
+    CoursesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
