c6c421feab72e97e769b1a2b4ba24292a1e65f6e
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-02-25 08:16:23 +0100

    util: add helper for displaying specializations

--- src/hbs/helpers.ts
+++ src/hbs/helpers.ts
@@ -1,3 +1,9 @@
+import { Specialization } from "src/specialization/schemas/specialization.schema";
+
 export function formatDate(dateObj: Date): string {
   return dateObj.toLocaleDateString();
+}
+
+export function formatSpecializations(specializations: Specialization[]) {
+  return specializations.map((s) => `<b>${s.name}</b>`).join(', ');
 }
