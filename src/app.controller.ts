d2a8c712a269d1bba8b36bf869f0b5de81e598a3
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-01 23:52:43 +0100

    style: remove unused import
    : title

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
-import { SpecializationService } from './specialization/specialization.service';
 
 @Controller()
 export class AppController {
